f528ea7dd9e8fb841640af87ebcb25f17a23caf6
dekopon21020014 <renueta@icloud.com>
2023-04-19 17:35:27 +0900

    remove

--- dfa-algorithms/sample.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-hoge = input()
-foo = input()
-
-print(hoge)
-print(foo)
-
-#with open('dfa-algorithms/test/01/dfa/all.txt', 'r') as dfa, open('dfa-algorithms/test/01/w/001.txt', 'r') as word:
